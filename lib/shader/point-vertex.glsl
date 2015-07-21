fc24f3ec1c90b1fadf723d597b6de1a034d17762
mikolalysenko <mikolalysenko@gmail.com>
2015-07-21 17:56:27 -0500

    gl-plot2d integration

--- lib/shader/point-vertex.glsl
+++ lib/shader/point-vertex.glsl
@@ -2,11 +2,11 @@ precision mediump float;
 
 attribute vec2 position;
 
-uniform mat3 modelViewProjection;
+uniform mat3 matrix;
 uniform float pointSize;
 
 void main() {
-  vec3 hgPosition = modelViewProjection * vec3(position, 1);
+  vec3 hgPosition = matrix * vec3(position, 1);
   gl_Position  = vec4(hgPosition.xy, 0, hgPosition.z);
   gl_PointSize = pointSize;
 }
--- scatter2d.js
+++ scatter2d.js
@@ -14,16 +14,18 @@ var SUBDIV_COUNT = 2
 
 module.exports = createScatter2D
 
-function Scatter2D(gl, offsetBuffer, shader) {
-  this.gl             = gl
+function Scatter2D(plot, offsetBuffer, shader) {
+  this.plot           = plot
   this.offsetBuffer   = offsetBuffer
   this.glyphSize      = 0
   this.shader         = shader
   this.scales         = []
   this.size           = 12.0
   this.borderSize     = 1.0
+  this.pointCount     = 0
   this.color          = [1,0,0,1]
   this.borderColor    = [0,0,0,1]
+  this.bounds         = [Infinity,Infinity,-Infinity,-Infinity]
 }
 
 var proto = Scatter2D.prototype
@@ -39,56 +41,84 @@ proto.update = function(options) {
   }
 
   this.size         = dflt('size', 12.0)
-  this.color        = dflt('color', [1,0,0,1])
+  this.color        = dflt('color', [1,0,0,1]).slice()
   this.borderSize   = dflt('borderSize', 1)
-  this.borderColor  = dflt('borderColor', [0,0,0,1])
+  this.borderColor  = dflt('borderColor', [0,0,0,1]).slice()
 
   //Update point data
   var data          = options.positions
-  this.scales       = preprocessPoints(data, SUBDIV_COUNT)
-  this.offsetBuffer.update(data)
+  var packed        = pool.mallocFloat32(data.length)
+  this.scales       = preprocessPoints(data, SUBDIV_COUNT, packed, this.bounds)
+  this.offsetBuffer.update(packed)
+  pool.free(packed)
+
+  this.pointCount = data.length >>> 1
 }
 
 function compareScale(a, b) {
   return b - a.scale
 }
 
-proto.draw = function(camera, scale, pixelRatio) {
-  var gl            = this.gl
-  var shader        = this.shader
-  var scales        = this.scales
-  var ext           = this.ext
-  var offsetBuffer  = this.offsetBuffer
+proto.draw = (function() {
+  var MATRIX = [1, 0, 0,
+                0, 1, 0,
+                0, 0, 1]
 
-  var pixelSize   = 0.5 / (gl.drawingBufferWidth * scale)
-  var targetScale = pixelSize / SUBDIV_COUNT
-  var scaleNum    = Math.min(Math.max(bsearch.le(scales, targetScale, compareScale), 0), scales.length-1)
+  return function() {
+    var plot          = this.plot
+    var shader        = this.shader
+    var scales        = this.scales
+    var offsetBuffer  = this.offsetBuffer
+    var bounds        = this.bounds
+    var size          = this.size
+    var borderSize    = this.borderSize
+    var gl            = plot.gl
+    var pixelRatio    = plot.pixelRatio
+    var viewBox       = plot.viewBox
+    var dataBox       = plot.dataBox
 
-  shader.bind()
-  shader.uniforms.modelViewProjection = camera
-  shader.uniforms.color               = this.color
-  shader.uniforms.pointSize           = (this.size + this.borderSize)
-  shader.uniforms.borderColor         = this.borderColor
+    var boundX  = bounds[2] - bounds[0]
+    var boundY  = bounds[3] - bounds[1]
+    var dataX   = dataBox[2] - dataBox[0]
+    var dataY   = dataBox[3] - dataBox[1]
+    var screenX = viewBox[2] - viewBox[0]
+    var screenY = viewBox[3] - viewBox[1]
 
-  shader.uniforms.screenSize = [gl.drawingBufferWidth, gl.drawingBufferHeight]
+    var pixelSize   = Math.max(dataX / screenX, dataY / screenY)
+    var targetScale = pixelSize
+    var scaleNum    = Math.min(Math.max(bsearch.le(scales, targetScale, compareScale), 0), scales.length-1)
 
-  if(this.borderSize === 0) {
-    shader.uniforms.centerFraction = 2.0;
-  } else {
-    shader.uniforms.centerFraction = this.size / (this.size + this.borderSize + 1.25)
+    MATRIX[0] = 2.0 * boundX / dataX
+    MATRIX[4] = 2.0 * boundY / dataY
+    MATRIX[6] = 2.0 * (bounds[0] - dataBox[0]) / dataX - 1.0
+    MATRIX[7] = 2.0 * (bounds[1] - dataBox[1]) / dataY - 1.0
+
+    shader.bind()
+    shader.uniforms.matrix      = MATRIX
+    shader.uniforms.color       = this.color
+    shader.uniforms.borderColor = this.borderColor
+    shader.uniforms.pointSize   = pixelRatio * (size + borderSize)
+
+    if(this.borderSize === 0) {
+      shader.uniforms.centerFraction = 2.0;
+    } else {
+      shader.uniforms.centerFraction = size / (size + borderSize + 1.25)
+    }
+
+    offsetBuffer.bind()
+    shader.attributes.position.pointer()
+    var offset = scales[scaleNum].count
+    var pointCount = this.pointCount
+    gl.drawArrays(gl.POINTS, offset, pointCount - offset)
   }
+})()
 
-  offsetBuffer.bind()
-  shader.attributes.position.pointer()
-
-  gl.drawArrays(gl.POINTS, 0, scales[scaleNum].count)
-}
-
-function createScatter2D(gl, options) {
+function createScatter2D(plot, options) {
+  var gl     = plot.gl
   var buffer = createBuffer(gl)
   var shader = createShader(gl, SHADERS.pointVertex, SHADERS.pointFragment)
 
-  var result = new Scatter2D(gl, buffer, shader)
+  var result = new Scatter2D(plot, buffer, shader)
   result.update(options)
   return result
 }
