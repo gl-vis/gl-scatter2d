0259c20918c4596ca30bdd9b6da94f6e62190201
Robert Monfera <monfera.robert@gmail.com>
2016-11-05 16:55:28 +0100

    improve projection precision

--- lib/shader/precise.glsl
+++ lib/shader/precise.glsl
@@ -1,7 +1,7 @@
 #pragma glslify: export(pfx)
 vec4 pfx(vec2 scaleHi, vec2 scaleLo, vec2 translateHi, vec2 translateLo, vec2 positionHi, vec2 positionLo) {
-  return vec4(scaleHi * positionHi + translateHi
-            + scaleLo * positionHi + translateLo
-            + scaleHi * positionLo
-            + scaleLo * positionLo, 0.0, 1.0);
+  return vec4((positionHi + translateHi) * scaleHi
+            + (positionLo + translateLo) * scaleHi
+            + (positionHi + translateHi) * scaleLo
+            + (positionLo + translateLo) * scaleLo, 0.0, 1.0);
 }
